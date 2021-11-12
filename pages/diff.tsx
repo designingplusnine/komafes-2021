ac01db3a8892914da826f9b41817f44b171aa715
khsacc <hiroki@eqchem.s.u-tokyo.ac.jp>
2021-11-13 04:22:49 +0900

    added each page

---
+++ pages/diff.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next';
+import styles from '../styles/pages/Home.module.scss';
+
+const Diff: NextPage = () => (
+  <div className={styles.container}>
+    <h1 className={styles.title}>Welcome to Komaba Festival exhibition by designing plus nine!</h1>
+  </div>
+);
+
+export default Diff;

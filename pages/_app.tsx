85d4e51985e62a09668b4440dec58c39bfaf63c5
khsacc <hiroki@eqchem.s.u-tokyo.ac.jp>
2021-11-13 21:37:11 +0900

    added and initialized next-seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,23 @@
 import '../styles/globals.scss';
+import { DefaultSeo } from 'next-seo';
 import { Footer } from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <DefaultSeo
+        openGraph={{
+          type: 'website',
+          locale: 'ja_JP',
+          url: 'http://dp9.tokyo/komafes2021/',
+          site_name: 'designing plus nine',
+        }}
+        twitter={{
+          handle: '@DP9_Official',
+          site: '@DP9_Official',
+          cardType: 'summary_large_image',
+        }}
+      />
       <Component {...pageProps} />
       <Footer />
     </>

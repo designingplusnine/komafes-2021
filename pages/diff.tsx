1a90646fc12a6601aabc806a73cca2a5d0ec6d39
khsacc <hiroki@eqchem.s.u-tokyo.ac.jp>
2021-11-14 02:15:41 +0900

    added contents components

--- pages/diff.tsx
+++ pages/diff.tsx
@@ -1,9 +1,10 @@
 import { NextPage } from 'next';
+import { Paper } from '@material-ui/core';
 import styles from '../styles/pages/Diff.module.scss';
 
 const Diff: NextPage = () => (
   <div className={styles.container}>
-    <h1 className={styles.title}>Welcome to Komaba Festival exhibition by designing plus nine!</h1>
+    <Paper></Paper>
   </div>
 );
 

4de0763b6da4a1f3526eb18aca595743bce8d5b6
khsacc <hiroki@eqchem.s.u-tokyo.ac.jp>
2021-11-14 14:51:58 +0900

    lint

--- components/TopPage/projects.tsx
+++ components/TopPage/projects.tsx
@@ -1,4 +1,4 @@
 import { NextPage } from 'next';
 import styles from '../styles/TopPage/concept.module.scss';
 
-export const Projects: NextPage = () =><div></div>;
+export const Projects: NextPage = () => <div></div>;

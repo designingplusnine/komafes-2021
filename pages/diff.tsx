c95333846594c5dfd79093bbb53ac36e60c6f84b
khsacc <hiroki@eqchem.s.u-tokyo.ac.jp>
2021-11-16 00:17:36 +0900

    added padding to paper component

--- pages/diff.tsx
+++ pages/diff.tsx
@@ -1,11 +1,12 @@
 import { Contents } from '../components/DiffPage/contents';
 import { NextPage } from 'next';
 import { Paper } from '@material-ui/core';
+import paperStyles from '../styles/pages/Paper.module.scss';
 import styles from '../styles/pages/Diff.module.scss';
 
 const Diff: NextPage = () => (
   <div className={styles.container}>
-    <Paper>
+    <Paper className={paperStyles.paper}>
       <Contents />
     </Paper>
   </div>

292a6bc15dc4a3b25eb6db2c1d374f78ae271248
Eyamada3 <yamada3ey@gmail.com>
2021-11-17 00:08:24 +0900

    writing the HTML for DiffPage.

--- components/DiffPage/contents.tsx
+++ components/DiffPage/contents.tsx
@@ -1,4 +1,27 @@
 import { NextPage } from 'next';
 import classes from './contents.module.scss';
 
-export const Contents: NextPage = () => <div></div>;
+export const Contents: NextPage = () => (
+    <div className={classes.page}>
+        <div className={classes.concept}>
+            <p className={classes.concept_text}>小企画説明文</p>
+            <hr className={classes.hr}></hr>
+        </div>
+        <div className={classes.time}>
+            <h1 className={classes.time_title}>時間？</h1>
+            <div className={classes.time_work}>
+                <img src=""></img>
+            </div>
+            <hr className={classes.hr}></hr>
+        </div>
+
+        <div className={classes.gravity}>
+            <h1 className={classes.gravity_title}>重力？</h1>
+            <hr className={classes.hr}></hr>
+        </div>
+
+        <div className={classes.unstable}>
+            <h1 className={classes.unstable_title}>不安定？</h1>
+        </div>
+    </div>
+);

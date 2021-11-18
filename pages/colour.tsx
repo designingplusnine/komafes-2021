266d03c486436aae9d2280e32c50389b9a1831ec
khsacc <hiroki@eqchem.s.u-tokyo.ac.jp>
2021-11-18 10:25:12 +0900

    added padding and margin to paper component in colour page

--- pages/colour.tsx
+++ pages/colour.tsx
@@ -1,10 +1,11 @@
 import { NextPage } from 'next';
 import { Paper } from '@material-ui/core';
 import classes from '../styles/pages/Colour.module.scss';
+import paperStyles from '../styles/pages/Paper.module.scss';
 
 const Colour: NextPage = () => (
   <div className={classes.container}>
-    <Paper>aaa</Paper>
+    <Paper className={paperStyles.paper}>aaa</Paper>
   </div>
 );
 

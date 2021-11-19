b01392488687eb626d24d899dca1f87934dcb394
NagiMasuda <calmix0630@gmail.com>
2021-11-20 08:59:17 +0900

    横井さんのコメントの反映（フォントサイズ以外）

--- components/TopPage/komafes.tsx
+++ components/TopPage/komafes.tsx
@@ -1,14 +1,13 @@
 import { NextPage } from 'next';
 import styles from './concept.module.scss';
 
-export const Komafes: NextPage = () => <div>
-    
-    <h1　className="deco1">駒場グランプリ</h1>
+export const Komafes: NextPage = () => (
+  <div>
+    <h2 className="deco1">駒場グランプリ</h2>
     <p>ご来場の皆さまの投票で、駒場祭で最も魅力的な企画を決定する</p>
-    <h2 className="deco2">駒場グランプリ</h2>
+    <h3 className="deco2">駒場グランプリ</h3>
     <p>が開催されています</p>
     <p>「ゆらぎ」の展示が良かったなと思っていただけましたら、以下のリンクまたはQRコードからぜひ投票をお願いします。</p>
-    <a href="https://www.komabasai.net/72/visitor/prix_hogehogehogehoge">https://www.komabasai.net/72/visitor/prix_hogehogehogehoge</a>
-
-    </div>;
-
+    <a href="投票はこちら">https://www.komabasai.net/72/visitor/prix_hogehogehogehoge</a>
+  </div>
+);

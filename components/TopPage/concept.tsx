1d68f9ffd4100769e88efbb1e905e78545cbf48b
sotaro-ha <alphacomplex.104729@gmail.com>
2021-11-19 21:52:15 +0900

    pull hirokik branch

--- components/TopPage/concept.tsx
+++ components/TopPage/concept.tsx
@@ -1,3 +1,37 @@
 import { NextPage } from 'next';
+import styles from './concept.module.scss';
 
-export const Concept: NextPage = () => <div></div>;
+const arr = [
+  { content: 'ゆらぎゆらめく時代に、', marginLeft: '10%' ,isFlag:true },
+  { content: 'わたしたちは生きている。', marginLeft: '10%' ,isFlag:true  },
+  { content: '不確実な世の中、', marginLeft: '10%' ,isFlag:false },
+  { content: '正解も不正解もわからない時代……', marginLeft: '10%'  ,isFlag:false},
+  { content: 'そんなことが言われて久しくなりました。', marginLeft: '10%' ,isFlag:true  },
+  { content: 'そして今、私たちを取り巻く不確かさは、', marginLeft: '10%'  ,isFlag:true },
+  { content: '日に日に増しているように感じられます。', marginLeft: '10%'  ,isFlag:true },
+  { content: '２つの対立するものが、', marginLeft: '10%'  ,isFlag:false },
+  { content: '混ざり、跳ね返り、また交わる。', marginLeft: '10%' ,isFlag:false },
+  { content: '私たちは、そんなゆらぎの中を', marginLeft: '10%' ,isFlag:true },
+  { content: '歩いていかなければならないのかもしれません。', marginLeft: '10%'  ,isFlag:true },
+  { content: 'しかしゆらぎは、', marginLeft: '10%'  ,isFlag:false },
+  { content: '自由がある証拠ともいえます。', marginLeft: '10%' ,isFlag:false,marginRight:'30%'  },
+  { content: 'そこにこそ、私たちは、新しい価値を', marginLeft: '10%' ,isFlag:false },
+  { content: '見出せるのではないでしょうか。', marginLeft: '30%' ,isFlag:false  },
+
+  { content: 'そんな世の中にあふれる', marginLeft: '10%' ,isFlag:true  },
+  { content: '「ゆらぎ」', marginLeft: '10%'  ,isFlag:true },
+  { content: 'を、デザインを通して考えました。', marginLeft: '10%' ,isFlag:true  },
+];
+
+
+export const Concept: NextPage = () => (
+  <div className={styles.concept}>
+    <div className={styles.concept__statement}>
+      {arr.map((paragraph, i) => (
+        <div style={ {marginLeft: paragraph.isFlag ? paragraph.marginLeft:"0%"} } className={paragraph.isFlag ? styles.concept__normal:styles.concept__indention} key={i}>
+          {paragraph.content}
+        </div>
+      ))}
+    </div>
+  </div>
+);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,17 @@
 import '../styles/globals.scss';
 import { DefaultSeo } from 'next-seo';
 import { Footer } from '../components/Footer';
-import { Header } from '../components/Header';
 import { useRouter } from 'next/dist/client/router';
-import Head from 'next/head';
 import classes from '../styles/pages/Layout.module.scss';
-
-export const colours = {
-  '/': 'f7fdfd',
-  '/colour': 'aec8d4',
-  '/time': 'f3e6e9',
-  '/diff': 'beb399',
-};
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-
+  const colours = {
+    '/': 'f7fdfd',
+    '/diff': 'beb399',
+    '/time': 'f3e6e9',
+    '/colour': 'aec8d4',
+  };
   return (
     <>
       <DefaultSeo
@@ -31,25 +27,10 @@ function MyApp({ Component, pageProps }) {
           cardType: 'summary_large_image',
         }}
       />
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-  (function(d) {
-    var config = {
-      kitId: 'hja2lby',
-      scriptTimeout: 3000,
-      async: true
-    },
-    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/%27+config.kitId+%27.js%27;tk.async=true;tk.onload=tk.onreadystatechange=function()%7Ba=this.readyState;if(f%7C%7Ca&&a!=%22complete%22&&a!=%22loaded%22)return;f=true;clearTimeout(t);try%7BTypekit.load(config)%7Dcatch(e)%7B%7D%7D;s.parentNode.insertBefore(tk,s)
-  })(document);`,
-          }}
-        ></script>
-      </Head>
-      <Header />
       <div className={classes.background__container} style={{ backgroundColor: `#${colours[router.pathname]}` }}>
         <Component {...pageProps} />
       </div>
+
       <Footer />
     </>
   );

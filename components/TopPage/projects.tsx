b3d51081cd570c7b523db3dec820b500ff7bf318
sotaro-ha <alphacomplex.104729@gmail.com>
2021-11-21 03:37:00 +0900

    add alt

--- components/TopPage/projects.tsx
+++ components/TopPage/projects.tsx
@@ -9,31 +9,31 @@ export const Projects: NextPage = () => (
     </div>
     <div className={styles.box}>
       <div className={styles.colour}>
-        <img src="/img/topPage/colour.png" className={styles.colour__img} />
+        <img src="/img/topPage/colour.png" className={styles.colour__img} alt="color" />
         <Link href="/colour">
         <a>
-        <img src="/img/topPage/colour.svg" className={styles.colour__button} />
+        <img src="/img/topPage/colour.svg" className={styles.colour__button}  alt="color"/>
         </a>
         </Link>
-        <img src="/img/landing/color_land.svg" className={styles.colour__land} />
+        <img src="/img/landing/color_land.svg" className={styles.colour__land} alt="color" />
       </div>
       <div className={styles.time}>
-        <img src="/img/topPage/time.png" className={styles.time__img} />
+        <img src="/img/topPage/time.png" className={styles.time__img} alt="time" />
         <Link href="/time">
         <a>
-        <img src="/img/topPage/time.svg" className={styles.time__button} />
+        <img src="/img/topPage/time.svg" className={styles.time__button}alt="time" />
         </a>
         </Link>
-        <img src="/img/landing/time_land.svg" className={styles.time__land} />
+        <img src="/img/landing/time_land.svg" className={styles.time__land} alt="time"/>
       </div>
       <div className={styles.diff}>
-        <img src="/img/topPage/diff.png" className={styles.diff__img} />
+        <img src="/img/topPage/diff.png" className={styles.diff__img} alt="diff"/>
         <Link href="/diff">
         <a>
-        <img src="/img/topPage/diff.svg" className={styles.diff__button} />
+        <img src="/img/topPage/diff.svg" className={styles.diff__button} alt="diff" />
         </a>
         </Link>
-        <img src="/img/landing/sense_land.svg" className={styles.diff__land} />
+        <img src="/img/landing/sense_land.svg" className={styles.diff__land} alt="diff" />
       </div>
     </div>
   </>

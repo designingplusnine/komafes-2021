9479cfe9a8acb55d591e992cb9123fed0025b26e
Hiroki Kobayashi <hiroki@eqchem.s.u-tokyo.ac.jp>
2021-11-20 22:41:19 +0900

    Revert "Hirokik"

---
+++ components/TimePage/timeData.ts
@@ -0,0 +1,17 @@
+export type TimeDatum = {
+  title: string;
+  category: string;
+  description: string;
+  designer: string;
+  image: string;
+};
+
+export const timeData: TimeDatum[] = [
+  {
+    title: 'タイトル',
+    category: '最新機器をレトロにデザイン',
+    description: '説明文だよ',
+    designer: 'hiroki',
+    image: '',
+  },
+];
--- components/TopPage/projects.tsx
+++ components/TopPage/projects.tsx
@@ -1,4 +1,4 @@
 import { NextPage } from 'next';
-import styles from './concept.module.scss';
+import styles from '../styles/TopPage/concept.module.scss';
 
-export const Projects: NextPage = () => <div>ここにプロジェクトが来ます</div>;
+export const Projects: NextPage = () => <div></div>;

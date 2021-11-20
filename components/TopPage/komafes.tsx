7f6756971aa4b678a7fe9657f6fc1e235e6de01a
sotaro-ha <alphacomplex.104729@gmail.com>
2021-11-20 12:20:58 +0900

    fix calmy

--- components/TopPage/komafes.tsx
+++ components/TopPage/komafes.tsx
@@ -1,13 +1,13 @@
 import { NextPage } from 'next';
-import styles from './concept.module.scss';
+import styles from './komafes.module.scss';
 
 export const Komafes: NextPage = () => (
   <div>
-    <h2 className="deco1">駒場グランプリ</h2>
-    <p>ご来場の皆さまの投票で、駒場祭で最も魅力的な企画を決定する</p>
-    <h3 className="deco2">駒場グランプリ</h3>
-    <p>が開催されています</p>
-    <p>「ゆらぎ」の展示が良かったなと思っていただけましたら、以下のリンクまたはQRコードからぜひ投票をお願いします。</p>
+    <h2 className={styles.deco1}>駒場グランプリ</h2>
+    <p className={styles.p}>ご来場の皆さまの投票で、駒場祭で最も魅力的な企画を決定する</p>
+    <h3 className={styles.deco2}>駒場グランプリ</h3>
+    <p className={styles.p}>が開催されています</p>
+    <p className={styles.p}>「ゆらぎ」の展示が良かったなと思っていただけましたら、以下のリンクまたはQRコードからぜひ投票をお願いします。</p>
     <a href="投票はこちら">https://www.komabasai.net/72/visitor/prix</a>
     {/* リンクがこれで良いか確認していただけると幸いです　*/}
   </div>

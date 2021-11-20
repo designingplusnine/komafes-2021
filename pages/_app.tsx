5b2fbc69b1a1b3169f2e8cac3bfc134377c50ad8
khsacc <hiroki@eqchem.s.u-tokyo.ac.jp>
2021-11-20 23:59:16 +0900

    removed unused scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,21 +31,6 @@ function MyApp({ Component, pageProps }) {
           cardType: 'summary_large_image',
         }}
       />
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-  (function(d) {
-    var config = {
-      kitId: 'hja2lby',
-      scriptTimeout: 3000,
-      async: true
-    },
-    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/%27+config.kitId+%27.js%27;tk.async=true;tk.onload=tk.onreadystatechange=function()%7Ba=this.readyState;if(f%7C%7Ca&&a!=%22complete%22&&a!=%22loaded%22)return;f=true;clearTimeout(t);try%7BTypekit.load(config)%7Dcatch(e)%7B%7D%7D;s.parentNode.insertBefore(tk,s)
-  })(document);`,
-          }}
-        ></script>
-      </Head>
       <Header />
       <div className={classes.background__container} style={{ backgroundColor: `#${colours[router.pathname]}` }}>
         <Component {...pageProps} />

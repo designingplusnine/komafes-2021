57f17d7972b1b980526dcaee2491b5b7ceeafb2e
NagiMasuda <calmix0630@gmail.com>
2021-11-20 19:42:39 +0900

    QR画像

--- components/TopPage/komafes.tsx
+++ components/TopPage/komafes.tsx
@@ -7,8 +7,13 @@ export const Komafes: NextPage = () => (
     <p className={styles.p}>ご来場の皆さまの投票で、駒場祭で最も魅力的な企画を決定する</p>
     <h3 className={styles.deco2}>駒場グランプリ</h3>
     <p className={styles.p}>が開催されています</p>
-    <p className={styles.p}>「ゆらぎ」の展示が良かったなと思っていただけましたら、以下のリンクまたはQRコードからぜひ投票をお願いします。</p>
-    <a href="投票はこちら">https://www.komabasai.net/72/visitor/prix</a>
+    <p className={styles.p}>
+      「ゆらぎ」の展示が良かったなと思っていただけましたら、以下のリンクまたはQRコードからぜひ投票をお願いします。
+    </p>
+    <a href="https://www.komabasai.net/72/visitor/prix">投票はこちら</a>
     {/* リンクがこれで良いか確認していただけると幸いです　*/}
+    <p className={styles.p}>
+      <img src="/public/img/topPage/QR.png"></img>
+    </p>
   </div>
 );

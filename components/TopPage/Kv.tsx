b5430f889b1848aa1e820e8261db9b8cb608903c
sotaro-ha <alphacomplex.104729@gmail.com>
2021-11-20 23:53:00 +0900

    Revert "add className"
    This reverts commit 5857a8eba179b145750bc9573418c3d745527a95.

--- components/TopPage/Kv.tsx
+++ components/TopPage/Kv.tsx
@@ -4,7 +4,7 @@ import Kv_png from "../../public/img/topPage/kv.png"
 import styles from './concept.module.scss';
 
 export const Kv: NextPage = () => (
-  <div className={styles.Kv}>
+  <div>
     <Image src={Kv_png} layout="intrinsic"/>
   </div>
 );

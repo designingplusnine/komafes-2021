aa6369c9fa1188c1423950940ea4b610ca50086a
Eyamada3 <yamada3ey@gmail.com>
2021-11-20 17:32:06 +0900

    reflected a bit on the comments

--- components/DiffPage/contents.tsx
+++ components/DiffPage/contents.tsx
@@ -2,26 +2,21 @@ import { NextPage } from 'next';
 import classes from './contents.module.scss';
 
 export const Contents: NextPage = () => (
-    <div className={classes.page}>
+    <>
         <div className={classes.concept}>
-            <p className={classes.concept_text}>小企画説明文</p>
-            <hr className={classes.hr}></hr>
+            <p className={classes.concept_text}>
+                「何かがおかしい」動画や写真を紹介しています。普段通りじゃない動きや振る舞いをするものを見つけてみてください。
+            </p>
         </div>
         <div className={classes.time}>
-            <h1 className={classes.time_title}>時間？</h1>
-            <div className={classes.time_work}>
-                <img src=""></img>
-            </div>
-            <hr className={classes.hr}></hr>
+            <h2 className={classes.time_title}>時間？</h2>
+            <div className={classes.time_work}></div>
         </div>
-
         <div className={classes.gravity}>
-            <h1 className={classes.gravity_title}>重力？</h1>
-            <hr className={classes.hr}></hr>
+            <h2 className={classes.gravity_title}>重力？</h2>
         </div>
-
         <div className={classes.unstable}>
-            <h1 className={classes.unstable_title}>不安定？</h1>
+            <h2 className={classes.unstable_title}>不安定？</h2>
         </div>
-    </div>
+    </>
 );

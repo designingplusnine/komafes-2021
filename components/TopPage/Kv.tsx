caaef4d9854db8359d8b0a61ae495a479049207f
sotaro-ha <alphacomplex.104729@gmail.com>
2021-11-20 23:24:50 +0900

    add Kv

--- components/TopPage/Kv.tsx
+++ components/TopPage/Kv.tsx
@@ -1,9 +1,10 @@
 import { NextPage } from 'next';
+import Image from 'next/image' ;
+import Kv_png from "../../public/img/topPage/kv.png"
 import styles from './concept.module.scss';
 
 export const Kv: NextPage = () => (
   <div>
-    {/* write here */}
-    <h1>ここにKVが来ます</h1>
+    <Image src={Kv_png} layout="intrinsic"/>
   </div>
 );

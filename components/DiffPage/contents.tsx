4ac4c012ef117068fc297ba82fd6cda116304007
Eyamada3 <yamada3ey@gmail.com>
2021-11-20 18:33:37 +0900

    add work comment

--- components/DiffPage/contents.tsx
+++ components/DiffPage/contents.tsx
@@ -10,7 +10,12 @@ export const Contents: NextPage = () => (
         </div>
         <div className={classes.time}>
             <h2 className={classes.time_title}>時間？</h2>
-            <div className={classes.time_work}></div>
+            <div className={classes.time_work}>
+                <p className={classes.time_work_comment}>
+                    時計は動く。規則正しく、正確に。早送りでも、逆再生でも、動画がカクカクでもお構いなしに。時間は決してゆらがない。時計は決して間違えない。
+                    再生時間の一分間、一定のリズムで、文字盤を一周、ぐるりと廻る……？
+                </p>
+            </div>
         </div>
         <div className={classes.gravity}>
             <h2 className={classes.gravity_title}>重力？</h2>

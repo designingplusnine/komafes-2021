dd7f3ebe6b9437d8b7f7f6c111f7a2faf780c0b7
khsacc <hiroki@eqchem.s.u-tokyo.ac.jp>
2021-11-21 02:08:51 +0900

    added tentative and minimum styles to each page

--- components/ColourPage/ColourItem.tsx
+++ components/ColourPage/ColourItem.tsx
@@ -4,20 +4,23 @@ import Image from 'next/image';
 import classes from './ColourItem.module.scss';
 
 export const ColourItem: NextPage<{ datum: ColourDatum }> = ({ datum }) => (
-  <div>
+  <div className={classes.container}>
     <h2>
       {datum.title}
       <span className={classes.category}>#{datum.category}</span>
     </h2>
+    <p style={{ lineHeight: 1.4 }}>
+      {datum.description.split('\n').map(l => (
+        <span key={l}>
+          {l}
+          <br />
+        </span>
+      ))}
+    </p>
     <div className={classes.content__container}>
-      <p style={{ lineHeight: 1.4 }}>
-        {datum.description.split('\n').map(l => (
-          <span key={l}>
-            {l}
-            <br />
-          </span>
-        ))}
-      </p>
+      {datum.after.map(image => (
+        <img className={classes.content__image} key={image} src={image} />
+      ))}
     </div>
   </div>
 );
--- components/TimePage/TimeContents.tsx
+++ components/TimePage/TimeContents.tsx
@@ -1,16 +1,25 @@
 import { NextPage } from 'next';
 import { TimeDatum, timeData } from './timeData';
+import Image from 'next/image';
 import classes from './TimeContent.module.scss';
 
 const TimeDatum: NextPage<{ datum: TimeDatum }> = ({ datum }) => (
-  <>
+  <div className={classes.container}>
     <h2>{datum.title}</h2>
     <div className={classes.content__container}>
       <div>
-        <p>{datum.description}</p>
+        <p style={{ lineHeight: 1.4 }}>
+          {datum.description.split('\n').map(l => (
+            <span key={l}>
+              {l}
+              <br />
+            </span>
+          ))}
+        </p>
+        <img src={datum.image} className={classes.img} />
       </div>
     </div>
-  </>
+  </div>
 );
 
 export const TimeContents: NextPage = () => (
